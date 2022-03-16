b300488c9416d7467d0005365f03b2eae47c1d7e
Mansur <funisinfinity26@gmail.com>
2022-03-16 21:33:23 +0100

    💥 change to node.js speech synthesis

--- electron/src/scripts/talk.js
+++ electron/src/scripts/talk.js
@@ -1,25 +1,5 @@
-function getRusVoice(voices){
-	for(let i in voices){
-		if(voices[i].lang.indexOf("ru") + 1){
-			return voices[i];
-		}
-	}
-	return null;
-}
+var tts = require('speech-synthesis');
 
-function say(text, lang="en"){
-    const voices = speechSynthesis.getVoices();
-    if(voices.length > 0){
-        const tts = new SpeechSynthesisUtterance(text);
-    
-        if(lang == "ru"){
-            tts.lang = "ru-RU";
-        }else if(lang == "en"){
-            tts.lang = "en-US"
-        }
-
-        if(!window.speechSynthesis.speaking && settings.voiceAssistant){
-            return window.speechSynthesis.speak(tts);
-        }
-    }
+function say(text, lang = "en") {
+    if (!tts.speaking) tts(text, "Alex");
 }
