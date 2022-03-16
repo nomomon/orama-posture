b0f45b392c84ea056ae82793174794e5953a3aa6
Mansur <funisinfinity26@gmail.com>
2022-03-16 21:34:18 +0100

    🎨 style code

--- electron/src/scripts/time.js
+++ electron/src/scripts/time.js
@@ -1,23 +1,23 @@
 let timeInMinutes = 0;
 
-function getHours(){
+function getHours() {
     return Math.floor(timeInMinutes / 60);
 }
 
-function getMinutes(){
+function getMinutes() {
     return Math.floor(timeInMinutes) % 60;
 }
 
-function updateClock(){
+function updateClock() {
     let h = getHours();
     let m = getMinutes();
-    if(h != 0 && m != 0){
-        $("#clock").innerText = `${h} hr ${m} min working`
-    }else if(h != 0 && m == 0){
-        $("#clock").innerText = `${h} hr working`
-    }else if(h == 0 && m != 0){
+    if (h != 0 && m != 0) {
+        $("#clock").innerText = `${h} hr${(h > 1) ? "s" : ""} ${m} min working`
+    } else if (h != 0 && m == 0) {
+        $("#clock").innerText = `${h} hr${(h > 1) ? "s" : ""} working`
+    } else if (h == 0 && m != 0) {
         $("#clock").innerText = `${m} min working`
-    }else if(h == 0 && m == 0){
+    } else if (h == 0 && m == 0) {
         $("#clock").innerText = `${0} min working`
     }
 }
